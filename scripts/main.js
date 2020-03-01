8c6240cb57accb4523e8622e9b4b67a11d71256b
DanG <gallagherd07@gmail.com>
2020-03-01 11:21:14 -0600

    Update JS

--- scripts/main.js
+++ scripts/main.js
@@ -1,9 +1,16 @@
-// NAVIGATION
-function myFunction() {
+///// NAVIGATION
+
+const BURGER = document.querySelector('.burger');
+
+function toggleNav() {
   var x = document.getElementById("myLinks");
   if (x.style.display === "block") {
     x.style.display = "none";
   } else {
     x.style.display = "block";
-  }
+  } BURGER.classList.toggle('fa-times');
 }
+
+(BURGER).addEventListener('click', function() {
+  toggleNav();
+});
