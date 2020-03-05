f02586ee44a575259579c6b788e9cdb9ad2958ea
DanG <gallagherd07@gmail.com>
2020-03-05 16:20:43 -0600

    Add Sticky Nav

--- scripts/main.js
+++ scripts/main.js
@@ -14,3 +14,17 @@ function toggleNav() {
 (BURGER).addEventListener('click', function() {
   toggleNav();
 });
+
+// STICKY NAV
+
+const BAR = document.querySelector('.sticky-header');
+const ARROW = document.querySelector('.arrow');
+
+(BAR).addEventListener('click', function() {
+  var x = document.querySelector('.sticky-links');
+  if (x.style.display === "block") {
+    x.style.display = "none";
+  } else {
+    x.style.display = "block";
+  } ARROW.classList.toggle('fa-angle-up');
+});
